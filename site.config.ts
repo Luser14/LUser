486ad8c4b070b5da3742a7f4573b6ffed78a2e24
Luser14 <examprep37@gmail.com>
2025-05-17 10:06:14 +0530

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -9,9 +9,9 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'L User',
+  domain: 'luser.vercel.app',
+  author: 'Luser',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
