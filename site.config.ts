a2e2a9db96c0ed9568320e180db02e560289bf07
Luser14 <examprep37@gmail.com>
2025-05-17 10:43:00 +0530

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -14,7 +14,7 @@ export default siteConfig({
   author: 'Luser',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'Debugging Life',
 
   // social usernames (optional)
   twitter: 'transitive_bs',
